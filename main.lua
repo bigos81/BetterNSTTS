30a544d3469d521431477d79d562af24ea40f56c
bigos <michal.rosikiewicz@gmail.com>
2026-04-22 13:56:04 +0200

    - version bump - added some other sound triggers

--- main.lua
+++ main.lua
@@ -30,6 +30,24 @@ function play_ogg(spell)
         PlaySoundFile("Interface\\AddOns\\Wildu_SharedMedia\\Media\\Sound\\Jenny\\2.ogg", "Master")
     elseif (string.find(spell, '3')) then
         PlaySoundFile("Interface\\AddOns\\Wildu_SharedMedia\\Media\\Sound\\Jenny\\3.ogg", "Master")
+    elseif (string.find(spell, 'explosion')) then
+        PlaySoundFile("Interface\\AddOns\\Wildu_SharedMedia\\Media\\Sound\\Jenny\\AoE.ogg", "Master")
+    elseif (string.find(spell, 'ironbark')) then
+        PlaySoundFile("Interface\\AddOns\\EnhanceQoLSharedMedia\\Sounds\\Voiceovers\\Ironbark.ogg", "Master")
+    elseif (string.find(spell, 'mass dispel')) then
+        PlaySoundFile("Interface\\AddOns\\SharedMedia_MyMedia\\sound\\mass-dispel.ogg", "Master")
+    elseif (string.find(spell, "cc adds")) then
+        PlaySoundFile("Interface\\AddOns\\EnhanceQoLSharedMedia\\Sounds\\Voiceovers\\CC.ogg", "Master")
+        C_Timer.After(0.8, function()
+            PlaySoundFile("Interface\\AddOns\\EnhanceQoLSharedMedia\\Sounds\\Voiceovers\\Adds.ogg", "Master")
+            end)
+    elseif (string.find(spell, "don't soak")) then
+        PlaySoundFile("Interface\\AddOns\\EnhanceQoLSharedMedia\\Sounds\\Voiceovers\\Don't.ogg", "Master")
+        C_Timer.After(0.6, function()
+            PlaySoundFile("Interface\\AddOns\\EnhanceQoLSharedMedia\\Sounds\\Voiceovers\\Soak.ogg", "Master")
+            end)
+    elseif (string.find(spell, 'soak')) then
+        PlaySoundFile("Interface\\AddOns\\EnhanceQoLSharedMedia\\Sounds\\Voiceovers\\Soak.ogg", "Master")
     else
         --fallback
         print('Unknown spell requested: '..spell)
