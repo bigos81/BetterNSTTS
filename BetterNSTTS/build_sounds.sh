f17d19b8ddd1c6eb457863213e1d4c49ab60eb2a
bigos <michal.rosikiewicz@gmail.com>
2026-05-05 09:48:42 +0200

    added ident in sounds.lua script

--- BetterNSTTS/build_sounds.sh
+++ BetterNSTTS/build_sounds.sh
@@ -2,6 +2,7 @@ rm sounds.lua
 touch sounds.lua
 echo '-- contains all the supported sounds (and file names of the .ogg files in media folder)' >> sounds.lua
 echo '-- as wow LUA prevents file existence checking, use build_sounds.sh script to auto generate data from folder' >> sounds.lua
+echo '-- this is automatically generated file' >> sounds.lua
 
 echo 'BetterNSTTS = {}' >> sounds.lua
 echo '' >> sounds.lua
@@ -9,7 +10,7 @@ echo 'BetterNSTTS.BNSTTS_SOUNDS =  {' >> sounds.lua
 
 find media -type f -name "*.ogg" | while read -r file; do
   filename=$(basename "$file" .ogg)
-  echo "[\"${filename}\"] = true," >> sounds.lua
+  echo -e "\t[\"${filename}\"] = true," >> sounds.lua
 done
 
 echo '}' >> sounds.lua
